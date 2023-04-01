e434a15ca7db156459770d6446c355f48218875b
mandi andrade <mandi.andrade20@gmail.com>
2023-04-01 17:03:09 -0500

    set global variables and converted data to object

--- script.js
+++ script.js
@@ -1,34 +1,55 @@
 $(document).ready(onReady);
 
+const monthlyBudget = 200000;
+let employeeData = [];
+
 function onReady() {
     console.log('Hey there! Everything is working just fine 😎');
     $('.submit-button').on('click', addEmployee);
     $('tbody').on('click', '.delete-button', fireEmployee);
+    let el = $('#budget-remaining');
+    el.empty();
+    el.append(monthlyBudget);
 }
 
 function addEmployee(event){
     // Need to stop the Form on HTML from doing its default behavior (refreshing, etc.):
     event.preventDefault();
-    console.log('You can add employee info now!');
+    
+    // GETTERS: 
     // reading the values in each field that user provides
-    let readFirstName = $('#first-name').val();
-    let readLastName = $('#last-name').val();
-    let readIdNum = $('#id-num').val();
-    let readJobTitle = $('#job-title').val();
-    let readSalary = $('#salary').val();
+    // UPDATE: turned this from separate variables into one object per employee! 
+    let newEmployee = {
+        firstName: $('#first-name').val(),
+        lastName: $('#last-name').val(),
+        idNum: $('#id-num').val(),
+        jobTitle: $('#job-title').val(),
+        salary: $('#salary')
+    }
+
+    // Pushing the data for each employee added into an array (global variable)
+    // Can't forget to test this in the console
+    employeeData.push(newEmployee);
+    console.log('New employee added:', newEmployee);
+    console.log('This is the whole employee list:', employeeData);
+
     // append the value that user provided to a new row in the table below (on the DOM)
-    // **had to use backticks for multiple line, & string interpolation for variable name**
+    // **had to use backticks for multiple line, & string interpolation for object.property name**
+    // NOTE: there appears to be a bug on line 46 with salary. In the DOM table, annual salary
+    // is being shown as [object Object]
     $('tbody').append(`
     <tr>
-        <td>${readFirstName}</td>
-        <td>${readLastName}</td>
-        <td>${readIdNum}</td>
-        <td>${readJobTitle}</td>
-        <td>${readSalary}</td>
+        <td>${newEmployee.firstName}</td>
+        <td>${newEmployee.lastName}</td>
+        <td>${newEmployee.idNum}</td>
+        <td>${newEmployee.jobTitle}</td>
+        <td>${newEmployee.salary}</td>
         <td class='delete-cell'><button class='delete-button'>🔥</button></td>
     </tr>
     `);
-    // now to clear the values in each field when the user clicks "submit" and data is put in 
+
+    // SETTERS:
+    //now to clear the values in each field when the user clicks "submit" and data is put in 
     // the table in the center of the DOM: 
     $('#first-name').val('');
     $('#last-name').val('');
