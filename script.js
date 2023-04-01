7c2923b6766b56a811058546dfdef134358a2b73
mandi andrade <mandi.andrade20@gmail.com>
2023-03-31 19:55:07 -0500

    css styling, stopped default button in js

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('Hey there! Everything is working just fine 😎');
+    $('.submit-button').on('click', addEmployee);
+}
+
+function addEmployee(event){
+    event.preventDefault();
+    console.log('You can add employee info now!');
 }
